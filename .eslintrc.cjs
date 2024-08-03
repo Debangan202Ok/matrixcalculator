8fcba90537355b61aaafb67d81e0a656c1c8abee
Debangan <98002712+Debangan202Ok@users.noreply.github.com>
2024-08-03 07:36:33 +0000

    -a

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
-// 👉 eslint setup for maintaining coding syle pattern with airbnb
+// 👉 eslint setup for maintaining coding style pattern with airbnb
 
 module.exports = {
   env: {
@@ -6,71 +6,67 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended',
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier',
-  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'prettier/prettier': 'warn',
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-    
-    // Disable prop-types as we use TypeScript for type checking
-    'react/prop-types': 'off',
+    "prettier/prettier": "warn",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
 
-    'import/no-extraneous-dependencies': [
-      'warn',
+    // Disable prop-types as we use TypeScript for type checking
+    "react/prop-types": "off",
+
+    "import/no-extraneous-dependencies": [
+      "warn",
       {
         devDependencies: [
-          '**/*.test.ts',
-          '**/*.test.tsx',
-          '**/*.spec.ts',
-          '**/*.spec.tsx',
-          '**/setupTests.ts',
+          "**/*.test.ts",
+          "**/*.test.tsx",
+          "**/*.spec.ts",
+          "**/*.spec.tsx",
+          "**/setupTests.ts",
         ],
       },
     ],
 
-    'import/prefer-default-export': 'off',
-    
-    'import/no-anonymous-default-export': 'off',
+    "import/prefer-default-export": "off",
 
-    'import/extensions': [
-      'warn',
-      'ignorePackages',
+    "import/no-anonymous-default-export": "off",
+
+    "import/extensions": [
+      "warn",
+      "ignorePackages",
       {
-        ts: 'never',
-        tsx: 'never',
-        js: 'never',
-        jsx: 'never',
+        ts: "never",
+        tsx: "never",
+        js: "never",
+        jsx: "never",
       },
     ],
 
     // Additional custom rules
-    'react/react-in-jsx-scope': 'off', // Not needed with React 17+
-    '@typescript-eslint/explicit-module-boundary-types': 'off', // Disable explicit return types on functions
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Ignore unused variables with an underscore
+    "react/react-in-jsx-scope": "off", // Not needed with React 17+
+    "@typescript-eslint/explicit-module-boundary-types": "off", // Disable explicit return types on functions
+    "@typescript-eslint/no-unused-vars": ["off"], // Disable check for unused variables
   },
   settings: {
     // Ensure ESLint can resolve TypeScript files
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
   },
